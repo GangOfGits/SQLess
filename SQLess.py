ab93ed8c4fa5a37c38d8dec49d77e13a2b7bea12
bench745 <35201139+bench745@users.noreply.github.com>
2018-11-16 11:45:29 +0000

    Update SQLess.py
    added to read_table

--- SQLess.py
+++ SQLess.py
@@ -1,36 +1,51 @@
 # SQLess Main Library file
 
 #A class fo using tables
-class table():
+class table(object):
     path = ''  # the location of a CSV file
     form = []  # the format of the CSV file (eg. ID, name, DoB,)
     content = [] # a 2d array, will be filled with sub arrays, each ub array is a row
 
-    def read(p): #read in a database from the path p, poupulate the variables content, form and path
+    def read_table(self, p): # read in a database from the path p, poupulate the variables content, form and path
+        try:
+            with open(p, 'r') as f:  # read in the content of a CSV file
+                stream = f.read()
+        except FileNotFoundError as E:
+            raise FileNotFoundError('No such path: {}'.format(p))
 
-
-    def new(self, f, p):  #creates a new CSV file with the format row f filled in at path p
+        stream = stream.split('\n')  # split the file by line
         
+        form = stream[0].split(',')  # split the first line by comma and add into the form variable
+
+        
+
+        
+
+
+    def new(self, f, p):  # creates a new CSV file with the format row f filled in at path p
+        pass
     
     def get(self, r, c):  # get at a value (r and c can be nubers for row and 
                           # column or some key and some feild name)
-
+        pass
 
     def update(self, r, c, v):  # fill in a value v (//)
-
+        pass
 
     def delete(self, r):  # remove row r (r can be a rown index ie. an int or it can be a key)
-
+        pass
 
     def insert(self, row_conntent):  # add a new row to bottom of the table, content in list form
+        pass
 
-
-    def query(self, cond = '', form_opts):  # SQL style queries with this table (ie. no FROM
-                                            # key word), eg:
+    def query(self, form_opts, cond = ''):  # SQL style queries with this table (ie. no FROM
+        pass                                # key word), eg:
                                             # table.query(" SELECT ID WHERE name = '%c' " , username)
 
 
     def save(self, p = path):  # save the current table at the path p 
-
+        pass
     
     
+t = table()
+t.read_table('test.txt')
